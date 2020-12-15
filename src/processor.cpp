ad555c71ef1f1b2cd8e84f4f54507daa2ea0c7d6
Shamil C <shamilcm@gmail.com>
2020-12-15 09:35:33 +0800

    added debug flag

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,24 +1,28 @@
+#include <chrono>
+#include <iostream> //DEBUG
 #include <map>
 #include <string>
 #include <string_view>
 #include <vector>
-#include <iostream> //DEBUG
 
 #include <pcrecpp.h>
 
-#include "utils.hpp"
 #include "langrules/rules.hpp"
 #include "processor.hpp"
+#include "utils.hpp"
 
 Processor::Processor(std::string text, const std::string &lang)
-    : text_(text), lang_(lang){
-      rules_ = Rules::CreateLangRules(lang_);
-    };
+    : text_(text), lang_(lang) {
+  rules_ = Rules::CreateLangRules(lang_);
+  debug_ = true;
+  rules_->debug_ = true;
+};
 
 pcrecpp::RE_Options Processor::options_ = pcrecpp::RE_Options(PCRE_UTF8);
 
 // Regex for matching sentences that end in fullstops.
-//const pcrecpp::RE Processor::re_fullstop = pcrecpp::RE("([^\\.]*\\.)", options_);
+// const pcrecpp::RE Processor::re_fullstop = pcrecpp::RE("([^\\.]*\\.)",
+// options_);
 
 // Returns a vector of string_view objects where each string_view object
 // represents an indivdual sentence.
@@ -26,35 +30,53 @@ std::vector<std::string> Processor::process() {
 
   std::vector<std::string> str_list;
   pcrecpp::StringPiece sent;
-  
+
+  const auto before = std::chrono::system_clock::now();
+
   // TODO: consider removing / explaining this
-  //std::string text_conv = text_sp.as_string(); //TODO: optimize
-  
+  // std::string text_conv = text_sp.as_string(); //TODO: optimize
+
   // std::cout << "before" << text_ << std::endl;
-  //pcrecpp::RE("\n").GlobalReplace("ABC", &text_) ; 
-  rules_->ApplyReplace("NewlineRule",text_);
+  // pcrecpp::RE("\n").GlobalReplace("ABC", &text_) ;
+  rules_->ApplyReplace("NewlineRule", text_);
 
   // (A) TODO: list replacement
-  
+
   // (B) abbreviation replacement
-  
-  // (1) Possessive Abbrevation: http://rubular.com/r/yqa4Rit8EY
-  // PossessiveAbbreviationRule = Rule(r"\.(?='s\s)|\.(?='s$)|\.(?='s\Z)", '∯')
+
+  // (1) Possessive Abbrevation: Jr.'s
   rules_->ApplyReplace("PossessiveAbbreviationRule", text_);
-  
-  // (2) self.lang.KommanditgesellschaftRule (Skipped because too specific) http://rubular.com/r/NEv265G2X2
-  
-  // (3) SingleUpperCaseLetterAtStartOfLineRule: https://rubular.com/r/e3H6kwnr6H
-  // Starting with a single upper case character followed by a period.
+
+  // (2) self.lang.KommanditgesellschaftRule (Skipped because too specific)
+  // http://rubular.com/r/NEv265G2X2
+
+  // (3) SingleUpperCaseLetterAtStartOfLineRule. Ex: Q. What is your name?
   rules_->ApplyReplace("SingleUpperCaseLetterAtStartOfLineRule", text_);
+
+  // (4) SingleUpperCaseLetterRule. Ex: John E. Smith.
+  rules_->ApplyReplace("SingleUpperCaseLetterRule", text_);
+
+  // (5) Prepositive Abbreviation
+  rules_->ApplyReplace("PrepositiveAbbreviationRule", text_);
+
+  // (6) Number Abbreviation
+  rules_->ApplyReplace("NumberAbbreviationRule", text_);
+
+  // (7) Other Abbreviation
+  rules_->ApplyReplace("AbbreviationRule", text_);
 
   // -- stub --
   pcrecpp::StringPiece text_sp = Utils::to_strpiece(text_);
   while (rules_->GetRuleRegex("NewLine").FindAndConsume(&text_sp, &sent)) {
     str_list.push_back(sent.as_string());
-
   }
-  
+
+  const std::chrono::duration<double> duration =
+      (std::chrono::system_clock::now() - before) * 1000;
+  if (debug_ == true) {
+    std::cerr << "In total, took " << duration.count() << "ms"
+              << std::endl; // DEBUG
+  }
 
   return str_list;
 }
