58b159d4e9fdc977b011bf0fd691f7725b458b03
Shamil C <shamilcm@gmail.com>
2020-12-15 09:34:02 +0800

    brace initialization

--- src/berpecah.cpp
+++ src/berpecah.cpp
@@ -8,18 +8,19 @@
 #include "berpecah.hpp"
 #include "processor.hpp"
 
-Berpecah::Berpecah(const std::string &lang) : lang_(lang) {
-}
+Berpecah::Berpecah(const std::string &lang) : lang_(lang) {}
 
 // function to segment
 std::vector<std::string> Berpecah::segment(std::string input_text) {
 
   // processor object
-  Processor processor { input_text, lang_};
+  Processor processor{input_text, lang_};
 
   std::vector<std::string> str_pieces;
 
   // process function
+
   str_pieces = processor.process();
+
   return str_pieces;
 };
