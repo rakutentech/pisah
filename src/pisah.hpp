884c8b6372ab5b51184026fd5e067d1bfb585cd6
Shamil C <shamilcm@gmail.com>
2021-03-23 17:41:33 +0800

    sample unit tests

--- src/pisah.hpp
+++ src/pisah.hpp
@@ -5,11 +5,11 @@
 
 class Pisah {
 private:
-  const std::string lang_;
+  std::string lang_;
 
 public:
-  Pisah(const std::string &lang, bool debug);
-  Pisah(const std::string &lang);
+  Pisah(std::string lang, bool debug);
+  Pisah(std::string lang);
   std::vector<std::string> Segment(std::string text);
   bool debug_;
 };
