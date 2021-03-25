badecc528caa21bcf66cd8a5b854f87664cbf73a
Shamil C <shamilcm@gmail.com>
2021-03-25 11:59:17 +0800

    remove sample codeA

--- test/lang/english.cpp
+++ test/lang/english.cpp
@@ -7,10 +7,7 @@
 #include "doctest.h"
 
 
-int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
-
 TEST_SUITE_BEGIN("English Tests");
-
 
 TEST_CASE("Golden Rule Tests") {
 
@@ -251,8 +248,5 @@ TEST_CASE("Golden Rule Tests") {
         // && std::equal(output.begin(), output.end(), expected.begin()));
     }
 
-    //CHECK(factorial(1) == 1);
-    //CHECK(factorial(2) == 2);
-    //CHECK(factorial(3) == 6);
-    //CHECK(factorial(10) == 3628800);
+
 }
